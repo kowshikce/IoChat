d7ce47aa62efc486f731ace5057b09d242837e9a
kowshikjr <kowshik.lm14@gmail.com>
2020-04-06 11:58:47 +0600

    added liveregistry

---
+++ app/src/main/java/com/example/iochat/ForegroundServices/LiveRegistry.java
@@ -0,0 +1,21 @@
+package com.example.iochat.ForegroundServices;
+
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
+public class LiveRegistry<K> extends LiveData<K>{
+
+
+
+
+    @Override
+    protected void onActive() {
+        super.onActive();
+    }
+
+    @Override
+    protected void onInactive() {
+        super.onInactive();
+    }
+}
